0b460d394780e339ddb1d58ba1b30043fa7136ac
Michael Barton <mail@michaelbarton.me.uk>
2013-05-22 12:33:19 -0400

    Add method to convert fastq lines into reads
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- src/fastq_features/core.clj
+++ src/fastq_features/core.clj
@@ -1,7 +1,20 @@
 (ns fastq_features.core
   (:gen-class)
-  (:use [clojure.tools.cli :only (cli)]))
+  (:require [clojure.string    :as   string])
+  (:use     [clojure.tools.cli :only (cli)]
+            [clojure.java.io]))
+
+(defn read-id [id]
+  (string/replace
+    (first (string/split id #"\s+"))
+    #"^@"
+    ""))
+
+(defn reads [fastq-lines]
+  (map (fn [[id s _ q]] {:id (read-id id) :sequence s :scores q})
+       (partition 4 fastq-lines)))
 
 (defn -main [& args]
-  (let [[options arguments] (cli args)] 
-    (println "Hellow world")))
+  (let [[options [feature fastq-file]] (cli args)
+         fastq-lines                   (line-seq (reader fastq-file))]
+    ))
---
+++ test/fastq_features/t_core.clj
@@ -0,0 +1,12 @@
+(ns fastq_features.t-core
+  (:use midje.sweet)
+  (:use [fastq_features.core]))
+
+(facts "about `reads`"
+
+  (fact "returns an empty list from an empty list"
+    (reads '()) => '())
+
+  (fact "returns a read from a single fastq entry"
+    (reads '("@SRR042334.1" "TGAC" "+SRR042334.1" "hhhh")) =>
+        '({:id "SRR042334.1" :scores "hhhh" :sequence "TGAC"})))
